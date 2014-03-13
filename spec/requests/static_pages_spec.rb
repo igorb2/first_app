d402bf960da04dd96142193c523c3fbe50672c1c
igorb2@thinknear.com <igorb2@MBR15-base-1091-2-2014.local>
2014-03-13 10:27:01 -0700

    finished static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,32 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the content and title 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+      expect(page).to have_title('| Sample App')
     end
   end
+  
+  describe "Help page" do
+
+    it "should have the content and title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+      expect(page).to have_title('| Help')
+    end
+  end
+  
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('| About Us')
+    end    
+  end  
 end
